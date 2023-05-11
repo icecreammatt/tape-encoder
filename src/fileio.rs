c0453df15ff47fa390b72229f476dc67cf14efa9
Matt Carrier <icecreammatt@gmail.com>
2023-05-10 23:55:50 -0700

    test: add coverage for path naming fixes

--- src/fileio.rs
+++ src/fileio.rs
@@ -3,8 +3,12 @@ use std::fs;
 use std::fs::File;
 use std::io::Write;
 
+fn file_to_hyphen(name: &String) -> String {
+    return String::from(name.replace("_", "-").replace(" ", "-").replace(".", "-"));
+}
+
 pub fn write_metadata(data: OutputMetadata) {
-    let directory = data.title.replace("_", "-").replace(" ", "-").replace(".", "-");
+    let directory = file_to_hyphen(&data.title);
 
     if !fs::metadata(&directory).is_ok() {
         fs::create_dir(&directory).unwrap();
@@ -14,4 +18,10 @@ pub fn write_metadata(data: OutputMetadata) {
     let outpath = format!("{}/{}.json", &directory, data.title);
     let mut file = File::create(outpath).unwrap();
     file.write_all(json.as_bytes()).unwrap();
+}
+
+#[test]
+fn file_to_hyphen_test() {
+    let result = file_to_hyphen(&"2022.04.06.name with space.mp4".to_string());
+    assert_eq!(result, "2022-04-06-name-with-space-mp4");
 }
